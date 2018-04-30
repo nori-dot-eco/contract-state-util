9d25d9c16ec00041030094dd806b63f07007c5a9
jaycenhorton <jaycen.horton@gmail.com>
2018-04-29 23:37:21 -0700

    allow dynamic artifact loc

--- .next/manifest.js
+++ .next/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "eaf89eca47f4ae11cc3c"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "c1500557e7e57b7a30b6"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","3":"bundles/pages/index.js"}[chunkId]||chunkId) + "-" + {"0":"dd3021d9528cb92a67db","1":"9a3a532253314e1f0220","3":"9fcdfada8c4c47b34511"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","3":"bundles/pages/index.js"}[chunkId]||chunkId) + "-" + {"0":"dd3021d9528cb92a67db","1":"9a3a532253314e1f0220","3":"5a40579e3feb37c33589"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
---
+++ bin/start.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const { exec } = require('child_process');
+const argv = require('yargs')
+  .usage('Usage: $0 [options]')
+  .alias('t', 'truffleConfigLoc')
+  .nargs('t', 1)
+  .describe('t', 'truffle.js location')
+  .demandOption(['t'])
+  .help('h')
+  .alias('h', 'help').argv;
+
+exec(
+  `node ./bin/setupArtifacts.js --truffleConfigLoc=${argv.truffleConfigLoc}`,
+  error => {
+    if (error !== null) {
+      console.log(`exec error: ${error}`);
+    }
+    exec('yarn next');
+  }
+);
